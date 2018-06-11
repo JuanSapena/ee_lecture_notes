87ae7ddaa59f85546a51693f372be21d5a0df8ac
ole-peters <ole.peters@zonlab.com>
2018-06-11 13:43:17 +0100

    tweaks to ch.4

--- chapter_1/figs/python/x_of_t_lin.py
+++ chapter_1/figs/python/x_of_t_lin.py
@@ -13,7 +13,7 @@ import pickle
 
 data_dir='./../../../../../../Dropbox/LML_research/ee_data'
 #
-#T = 52
+T = 52 +1
 #N_ensemble=1000000
 #
 #ensemble = pd.DataFrame(index=np.arange(0,N_ensemble),columns=np.arange(0,T))
@@ -25,13 +25,14 @@ data_dir='./../../../../../../Dropbox/LML_research/ee_data'
 #    # 50% chance of 1.5x what we had before.
 #    ensemble.iloc[:,t]=ensemble.iloc[:,t-1] * np.random.choice([0.6, 1.5],size=N_ensemble)
 #ensemble.to_pickle(data_dir+"coin_ensemble.pkl")
-#ensemble=pd.read_pickle(data_dir+"coin_ensemble.pkl")
+ensemble=pd.read_pickle(data_dir+"coin_ensemble.pkl")
 
 x = np.arange(T)
 plt.plot(x, ensemble.iloc[45,:], 'b-', label='$N=1$')
 plt.plot(x, np.mean(ensemble.iloc[0:100,:]), 'g-', label='$N=100$')
 plt.plot(x, np.mean(ensemble.iloc[0:10000,:]), 'r-', label='$N=10,000$')
 plt.plot(x, np.mean(ensemble), 'k-', label='$N=1,000,000$')
+plt.xlim((0,max(ensemble.columns)))
 plt.legend()
 plt.xlabel('$t$')
 plt.ylabel('$x(t)$')
