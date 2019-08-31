fa64d2519febd2b3c6b140beffae9def2ebb1e25
ole-peters <ole.peters@zonlab.com>
2019-08-31 20:21:18 +0200

    ok version of riskless

--- chapter_riskless/figs/python/disc_3.py
+++ chapter_riskless/figs/python/disc_3.py
@@ -15,15 +15,16 @@ b_u=1
 C=1
 x=np.arange(-.1,2.,.01)
 
-plt.plot([1, 1], [0, 1], 'k-', lw=3)
-plt.plot([2, 2], [0, 3], 'k-', lw=3)
-plt.plot([0.75, 2], [0, 3], 'r:', lw=4)
-plt.plot([0.75, 1], [0, 1], 'b:', lw=3)
+plt.plot([1, 1], [0, 10], 'k-', lw=3)
+plt.plot([2, 2], [0, 25], 'k-', lw=3)
+plt.plot([2./3, 2], [0, 25], 'r:', lw=4)
+plt.plot([2./3, 1], [0, 10], 'b:', lw=3)
+
 plt.rc('xtick',labelsize=16)
 plt.rc('ytick',labelsize=16)
 plt.gca().spines['right'].set_color('none')
 plt.gca().spines['top'].set_color('none')
 plt.xticks([0,1,2], ['1 Jul','1 Aug','1 Sep'], rotation=0)
-plt.yticks([0,1,2,3], ['','$1','$2','$3'], rotation=0)
+plt.yticks([0,10,20,30], ['','$10','$20','$30'], rotation=0)
 plt.savefig("./../disc_3.pdf", bbox_inches='tight')
 plt.show()
