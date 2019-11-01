7e86039c65091d8a153ca7e7de54ba1bbde3bdab
ole-peters <ole.peters@zonlab.com>
2019-11-01 17:20:30 -0600

    new figures for chapter_real

--- chapter_real/figs/python/eta_constraint.py
+++ chapter_real/figs/python/eta_constraint.py
@@ -8,6 +8,14 @@ Created on Thu Sep  5 11:06:55 2019
 import matplotlib.pyplot as plt
 import numpy as np
 
+SCfigure=True
+if SCfigure:
+    font = {'family' : 'normal',
+        'weight' : 'normal',
+        'size'   : 15}
+    plt.rc('font', **font)
+
+
 def v(eta,x):
     v=(np.power(x,(1-eta))-1)/(1-eta)
     return v
@@ -33,16 +41,17 @@ for x in eta:
 #plt.plot(t,wealth_co,'b-',linewidth=3, label=r'$y^{(2)}(t)$')
 #plt.plot(t,wealth_1,color='#00ff00',linewidth=3, label=r'$x_1(t)$')
 plt.plot(eta,magnitude,color='#0000FF',linewidth=2, label=r'$x_1(t)$')
-plt.plot(eta,zero,color='#000000',linewidth=2, label=r'$x_1(t)$')
+#plt.plot(eta,zero,color='#000000',linewidth=2, label=r'$x_1(t)$')
 #plt.plot(t,wealth_2,color='#55bb55',linewidth=3, label=r'$x_2(t)$')
 #plt.plot(t,wealth_ave,'k-',linewidth=1, label=r'$(x_1(t)+x_2(t))/2$')
 
 plt.gca().spines['right'].set_color('none')
 plt.gca().spines['top'].set_color('none')
+plt.gca().spines['bottom'].set_position('zero')
 
 #plt.yscale('log')
 #plt.legend()
-plt.xlabel('parameter $\eta$')
+plt.xlabel('concavity parameter $\eta$')
 plt.ylabel('inequality magnitude')
 #
 #plt.yticks([1,10.**10.,10.**20.,10.**30.,10.**40.],\
