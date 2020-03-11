c5435aebeffcaa1c665a72f8fc2c3a3f737cb745
ole-peters <ole-peters@users.noreply.github.com>
2020-03-11 18:01:12 +0200

    markets
    figure update, section named Markowitz

--- chapter_markets/figs/python/fig_rebalance.py
+++ chapter_markets/figs/python/fig_rebalance.py
@@ -51,39 +51,13 @@ ax1.fill_betweenx([0,.3*15.3],5,5.2,color=(1,0,0,1))
 ax1.fill_betweenx([0,.7*15.3],5.4,5.6,color=(0,0,1,1))
 #l=.7
 
-
-plt.axhline(y=0,linestyle=':',color='grey',linewidth=.5)
-#ax1.plot(final_equity_1, label='S&P500TR',linewidth=2,color='blue')
-#ax1.plot(final_equity_3, label='DAX',linewidth=2,color='orange')
-#ax2=ax1.twinx()
-#ax2.plot(final_equity_2, label='Bitcoin',linewidth=2,color='red')
-#ax1.set_yscale('log')
-#ax2.set_yscale('log')
 ax1.set_xlim([-2,7])
 ax1.set_ylim([0,15])
-#ax2.set_ylim([0.1**5,10**7])
-#plt.xlim([final_equity_1.index.min(),final_equity_1.index.max()])
-#plt.ylim([-1.5,2.2])
-#vals = ax.get_yticks()
-#ax.set_yticklabels(['{:3.0f}% p.a.'.format(100*x) for x in vals])
-#ax.set_aspect(1./(1.618*ax.get_data_ratio()))
-#plt.title('some title')
-#ax1.set_xlabel('day')
-#ax1.set_ylabel('holding')
-#ax2.set_ylabel('final equity (BTC)',color='red')
-plt.tick_params(\
-    axis='both',          # changes apply to the x-axis
-    which='both',      # both major and minor ticks are affected
-    bottom='off',      # ticks along the bottom edge are off
-    top='off',         # ticks along the top edge are off
-    labelbottom='off',
-    right='off',
-    left='off',
-    labelleft='off'
-    )
-#plt.axvline(x=1.68045,linestyle='--',color='grey',linewidth=1)
-#ax1.legend(loc=4, bbox_to_anchor=(.93,.5))
-#ax1.legend(loc=4, bbox_to_anchor=(.38,.8))
-#ax.legend_.remove()
+ax1.set_yticks([])
+ax1.set_xticks([])
+
+ax1.spines['right'].set_color('none')
+ax1.spines['left'].set_color('none')
+ax1.spines['top'].set_color('none')
 plt.savefig("../rebalance.pdf", bbox_inches='tight')
 plt.show()
